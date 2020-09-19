c8c4df00fed56f8d83d57f9ef622bd4ff0cae257
ajaykumaresh <ajaykumaresh9@gmail.com>
2020-09-19 11:59:15 +0530

    Not found page and footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer=()=>{
     return(
     <div className="col-sm-12" style={{width:"100%", heigth:"20px",borderTop: '1px solid #676363'}}>
-        <p style={{textAlign: 'center',fontSize:'10px', paddingTop: '10px'}}>&copy; Copyright 2020, Ajay Corporation</p>
+        <p style={{textAlign: 'center',fontSize:'10px', paddingTop: '10px'}}>&copy; Copyright 2020 | All rights reserved</p>
         </div>
     )
 }
---
+++ src/components/notFound/pagenotfound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './notfound.css'
+const NotFound=(props)=>{
+ return( <div class="mainbox">
+    <div class="err">4</div>
+    <i class="far fa-question-circle fa-spin"></i>
+    <div class="err2">4</div>
+    <div class="msg">OOPS!! No Procucts Found..Maybe your filters are High to Process!!! Please reduce filters properties to show Products.</div>
+      </div>
+ )
+}
+
+export default NotFound;
