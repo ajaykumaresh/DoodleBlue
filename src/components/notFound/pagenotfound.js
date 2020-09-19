9718b0f03cf97df6cd88fa0125693dd529c8f6ac
ajaykumaresh <ajaykumaresh9@gmail.com>
2020-09-19 12:08:23 +0530

    class to classname in footer

--- src/components/notFound/pagenotfound.js
+++ src/components/notFound/pagenotfound.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './notfound.css'
 const NotFound=(props)=>{
- return( <div class="mainbox">
-    <div class="err">4</div>
-    <i class="far fa-question-circle fa-spin"></i>
-    <div class="err2">4</div>
-    <div class="msg">OOPS!! No Procucts Found..Maybe your filters are High to Process!!! Please reduce filters properties to show Products.</div>
+ return( <div className="mainbox">
+    <div className="err">4</div>
+    <i className="far fa-question-circle fa-spin"></i>
+    <div className="err2">4</div>
+    <div className="msg">OOPS!! No Procucts Found..Maybe your filters are High to Process!!! Please reduce filters properties to show Products.</div>
       </div>
  )
 }
